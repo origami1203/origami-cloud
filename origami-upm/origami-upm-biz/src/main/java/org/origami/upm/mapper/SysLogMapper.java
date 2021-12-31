cf69dec01f10c71939e8e34785949e74de290410
origami <mcxyml123@163.com>
2021-12-31 12:23:09 +0800

    系统日志

---
+++ origami-upm/origami-upm-biz/src/main/java/org/origami/upm/mapper/SysLogMapper.java
@@ -0,0 +1,13 @@
+package org.origami.upm.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.origami.upm.api.entity.SysLog;
+
+/**
+ * 系统操作日志mapper
+ *
+ * @author origami
+ * @date 2021/12/31 12:06
+ */
+public interface SysLogMapper extends BaseMapper<SysLog> {
+}
