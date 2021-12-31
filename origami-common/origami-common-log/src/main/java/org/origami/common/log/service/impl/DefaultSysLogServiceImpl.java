41a072770533f653aea7f28a3f5210f991abec69
origami <mcxyml123@163.com>
2022-01-01 04:06:11 +0800

    添加注释

--- origami-common/origami-common-log/src/main/java/org/origami/common/log/service/impl/DefaultSysLogServiceImpl.java
+++ origami-common/origami-common-log/src/main/java/org/origami/common/log/service/impl/DefaultSysLogServiceImpl.java
@@ -10,6 +10,10 @@ import org.origami.upm.api.entity.SysLog;
  * @date 2022/1/1 3:57
  */
 public class DefaultSysLogServiceImpl implements SysLogService {
+    /**
+     * 默认不做处理，可自定义实现
+     * @param sysLog 日志
+     */
     @Override
     public void handle(SysLog sysLog) {
        // 空处理
