25c84d06fb8cbfb262bcdd46b0c85be377099dee
苑梦龙 <yuanmenglong@qidu.com>
2021-12-31 14:48:23 +0800

    格式

--- origami-upm/origami-upm-biz/src/main/java/org/origami/upm/OrigamiUmpApplication.java
+++ origami-upm/origami-upm-biz/src/main/java/org/origami/upm/OrigamiUmpApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 权限管理biz
+ *
  * @author origami
  * @date 2021/12/31 12:03
  */
 @SpringBootApplication
-public class    OrigamiUmpApplication {
+public class OrigamiUmpApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrigamiUmpApplication.class, args);
     }
