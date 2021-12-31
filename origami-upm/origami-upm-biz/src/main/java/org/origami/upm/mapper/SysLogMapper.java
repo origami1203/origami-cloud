fa11e973edc1e8e0d31e250f625772d004ce8cdb
苑梦龙 <yuanmenglong@qidu.com>
2021-12-31 11:29:06 +0800

    系统日志mapper

---
+++ origami-upm/origami-upm-biz/src/main/java/org/origami/upm/mapper/SysLogMapper.java
@@ -0,0 +1,14 @@
+package org.origami.upm.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.origami.common.log.annotation.SysLog;
+
+/**
+ * 系统操作日志mapper
+ *
+ * @author yuanmenglong
+ * @version 1.0.0
+ * @date 2021-12-31 11:28
+ */
+public interface SysLogMapper extends BaseMapper<SysLog> {
+}
