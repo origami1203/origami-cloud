4cf7b66beb508e7f8d338bc590785a44bfcb11c0
origami <mcxyml123@163.com>
2022-01-01 00:38:42 +0800

    @SysLog可被标注到类上

--- origami-common/origami-common-log/src/main/java/org/origami/common/log/annotation/SysLog.java
+++ origami-common/origami-common-log/src/main/java/org/origami/common/log/annotation/SysLog.java
@@ -7,18 +7,23 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 系统操作日志
+ * 记录系统操作日志
+ * 标注于类上，类中所有方法记录操作日志
+ * 标注方法上，指定方法被记录操作日志
  *
  * @author origami1203
  * @date 2021/12/30 19:29
  */
 @Documented
-@Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface SysLog {
     
     /**
-     * 方法描述
+     * 方法的描述，
+     * 为空则尝试寻找方法上是否有@ApiOperation注解，若有，尝试获取其value()
+     * 两者都没有,则返回被调用方法的方法名
+     *
      * @return string
      */
     String value() default "";
