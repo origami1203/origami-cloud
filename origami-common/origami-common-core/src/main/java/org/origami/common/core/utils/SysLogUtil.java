e4d49fc2df898d719d57a35d46e2cdba2cca2ae1
origami <mcxyml123@163.com>
2021-12-31 08:07:27 +0800

    新增工具类

---
+++ origami-common/origami-common-core/src/main/java/org/origami/common/core/utils/SysLogUtil.java
@@ -0,0 +1,16 @@
+package org.origami.common.core.utils;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ * 系统操作日志工具类
+ *
+ * @author origami
+ * @date 2021/12/30 22:30
+ */
+@UtilityClass
+public class SysLogUtil {
+    
+}
---
+++ origami-common/origami-common-core/src/main/java/org/origami/common/core/utils/UserContextUtil.java
@@ -0,0 +1,23 @@
+package org.origami.common.core.utils;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Objects;
+
+/**
+ * 用户信息
+ *
+ * @author origami
+ * @date 2021/12/30 22:34
+ */
+@UtilityClass
+public class UserContextUtil {
+    
+    public String getUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return Objects.nonNull(authentication) ? authentication.getName() : "null";
+    }
+}
