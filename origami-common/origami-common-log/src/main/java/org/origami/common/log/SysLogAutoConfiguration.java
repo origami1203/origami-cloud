61132e93da893af51b12fc24b4442c2f63e012eb
origami <mcxyml123@163.com>
2022-01-01 13:35:57 +0800

    添加debug日志消息

--- origami-common/origami-common-log/src/main/java/org/origami/common/log/SysLogAutoConfiguration.java
+++ origami-common/origami-common-log/src/main/java/org/origami/common/log/SysLogAutoConfiguration.java
@@ -1,5 +1,6 @@
 package org.origami.common.log;
 
+import lombok.extern.slf4j.Slf4j;
 import org.origami.common.log.aspect.SysLogAspect;
 import org.origami.common.log.service.SysLogInfoService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
  * @version 1.0.0
  * @date 2021-12-31 11:02
  */
+@Slf4j
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnWebApplication
 public class SysLogAutoConfiguration {
@@ -26,6 +28,6 @@ public class SysLogAutoConfiguration {
     @Bean
     @ConditionalOnMissingBean(SysLogInfoService.class)
     public SysLogInfoService sysLogService() {
-        return log -> {};
+        return sysLogInfo -> log.debug("收到了一个系统操作日志：{}，然后把它丢弃了", sysLogInfo);
     }
 }
