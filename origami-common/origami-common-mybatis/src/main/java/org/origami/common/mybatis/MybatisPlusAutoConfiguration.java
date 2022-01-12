5d4e753cd55e7cdabf5d2196f3b8a2106c198bad
origami <mcxyml123@163.com>
2022-01-12 08:19:12 +0800

    自定义主键生成策略

--- origami-common/origami-common-mybatis/src/main/java/org/origami/common/mybatis/MybatisPlusAutoConfiguration.java
+++ origami-common/origami-common-mybatis/src/main/java/org/origami/common/mybatis/MybatisPlusAutoConfiguration.java
@@ -1,7 +1,9 @@
 package org.origami.common.mybatis;
 
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
 import org.origami.common.mybatis.config.MybatisPlusMetaObjectHandler;
 import org.origami.common.mybatis.plugins.MybatisPlusPlugins;
+import org.origami.common.mybatis.utils.SnowflakeIdGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -14,11 +16,16 @@ import org.springframework.context.annotation.Import;
  * @date 2021-12-29 19:58
  */
 @Import(MybatisPlusPlugins.class)
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class MybatisPlusAutoConfiguration {
     
     @Bean
     public MybatisPlusMetaObjectHandler metaObjectHandler() {
         return new MybatisPlusMetaObjectHandler();
+    }
+    
+    @Bean
+    public IdentifierGenerator identifierGenerator() {
+        return new SnowflakeIdGenerator();
     }
 }
