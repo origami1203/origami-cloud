e0a530b78bcc4dd9fea21bd007afbf904eadd26b
苑梦龙 <yuanmenglong@qidu.com>
2022-01-14 12:47:04 +0800

    设置base自动填充

--- origami-common/origami-common-mybatis/src/main/java/org/origami/common/mybatis/entity/BaseEntity.java
+++ origami-common/origami-common-mybatis/src/main/java/org/origami/common/mybatis/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package org.origami.common.mybatis.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
@@ -28,30 +29,34 @@ public abstract class BaseEntity {
      * 创建时间
      */
     @ApiModelProperty(value = "创建日期")
+    @TableField(fill = FieldFill.INSERT)
     protected LocalDateTime createDate;
     /**
      * 最后更新时间
      */
     @ApiModelProperty(value = "更新日期")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     protected LocalDateTime updateDate;
     /**
      * 创建人
      */
     @ApiModelProperty(value = "创建人")
+    @TableField(fill = FieldFill.INSERT)
     protected String createBy;
     /**
      * 最后更新人
      */
     @ApiModelProperty(value = "最后更新人")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     protected String updateBy;
     /**
      * 逻辑删除
      */
-    @TableLogic
+    @TableLogic(value = "0", delval = "1")
     @ApiModelProperty(value = "逻辑删除")
     @TableField(value = "is_deleted")
     protected Boolean deleted;
-    
+
     /**
      * 乐观锁
      */
