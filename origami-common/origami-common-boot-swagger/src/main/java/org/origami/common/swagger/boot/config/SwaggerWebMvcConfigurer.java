02e59ed0b74f409f2c6d0cc68e3003f7d4d6eaf3
苑梦龙 <yuanmenglong@qidu.com>
2022-01-17 13:59:31 +0800

    webmvc配置转发及资源映射

---
+++ origami-common/origami-common-boot-swagger/src/main/java/org/origami/common/swagger/boot/config/SwaggerWebMvcConfigurer.java
@@ -0,0 +1,28 @@
+package org.origami.common.swagger.boot.config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 配置url映射和资源映射
+ *
+ * @author yuanmenglong
+ * @version 1.0.0
+ * @date 2022-01-17 13:07
+ */
+public class SwaggerWebMvcConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.
+                addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+                .resourceChain(false);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/swagger-ui/")
+                .setViewName("forward:/swagger-ui/index.html");
+    }
+}
