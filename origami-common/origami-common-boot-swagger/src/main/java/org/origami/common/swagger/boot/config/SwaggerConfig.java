05f3957fc8fc5a7081731d56662610b720691ff8
苑梦龙 <yuanmenglong@qidu.com>
2022-01-17 14:02:30 +0800

    取消自动配置,使用自定义webmvc配置

--- origami-common/origami-common-boot-swagger/src/main/java/org/origami/common/swagger/boot/config/SwaggerConfig.java
+++ origami-common/origami-common-boot-swagger/src/main/java/org/origami/common/swagger/boot/config/SwaggerConfig.java
@@ -1,11 +1,11 @@
 package org.origami.common.swagger.boot.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.boot.starter.autoconfigure.SwaggerUiWebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -26,26 +26,27 @@ import java.util.List;
  * @version 1.0.0
  * @date 2022-01-07 08:56
  */
-@Configuration
+@Slf4j
 @EnableOpenApi
+@Configuration
 @ConditionalOnWebApplication
 @EnableConfigurationProperties(SwaggerProperties.class)
-@ConditionalOnProperty(prefix = "swagger", name = "enabled", matchIfMissing = true, havingValue = "true")
+@ConditionalOnProperty(prefix = "swagger", name = "enabled", havingValue = "true")
 public class SwaggerConfig {
-    
+
     /**
      * 默认的排除路径，排除Spring Boot默认的错误处理路径和端点
      */
     private static final List<String> DEFAULT_EXCLUDE_PATH =
             Arrays.asList("/error", "/actuator/**");
-    
+
     private static final String BASE_PATH = "/**";
-    
+
     @Bean
     public Docket api(SwaggerProperties properties) {
-    
-        System.out.println("11111111111111111111111111111111111111111111111111");
-        
+
+        log.debug("开启swagger-ui");
+
         if (properties.getBasePath().isEmpty()) {
             properties.getBasePath().add(BASE_PATH);
         }
@@ -55,16 +56,16 @@ public class SwaggerConfig {
                 .apiInfo(apiInfo(properties))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(properties.getBasePackage()));
-        
+
         properties.getBasePath().stream().map(PathSelectors::ant).forEach(builder::paths);
         properties.getExcludePath()
-                  .stream()
-                  .map(excludePath -> PathSelectors.ant(excludePath).negate())
-                  .forEach(builder::paths);
-        
+                .stream()
+                .map(excludePath -> PathSelectors.ant(excludePath).negate())
+                .forEach(builder::paths);
+
         return builder.build();
     }
-    
+
     @Bean
     public ApiInfo apiInfo(SwaggerProperties properties) {
         return new ApiInfoBuilder()
@@ -79,16 +80,10 @@ public class SwaggerConfig {
                 .termsOfServiceUrl(properties.getTermsOfServiceUrl())
                 .build();
     }
-    
+
     @Bean
-    public SwaggerUiWebMvcConfigurer swaggerUiConfigurer(SwaggerProperties properties) {
-        return new SwaggerUiWebMvcConfigurer("");
+    public SwaggerWebMvcConfigurer swaggerUiConfigurer() {
+        return new SwaggerWebMvcConfigurer();
     }
-    
-    // @Bean
-    // @ConditionalOnMissingBean
-    // public SwaggerProperties swaggerProperties() {
-    //     return new SwaggerProperties();
-    // }
-    
+
 }
