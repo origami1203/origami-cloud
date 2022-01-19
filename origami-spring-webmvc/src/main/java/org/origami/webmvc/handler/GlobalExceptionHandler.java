a29ad46024c43cf1e32b58d28bcf1b62e963ac20
origami <mcxyml123@163.com>
2022-01-19 12:29:32 +0800

    更新全局异常处理

--- origami-spring-webmvc/src/main/java/org/origami/webmvc/handler/GlobalExceptionHandler.java
+++ origami-spring-webmvc/src/main/java/org/origami/webmvc/handler/GlobalExceptionHandler.java
@@ -1,10 +1,18 @@
 package org.origami.webmvc.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.origami.common.core.base.Result;
-import org.origami.common.core.exception.IllegalParamException;
 import org.origami.common.core.exception.base.BaseException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.stream.Collectors;
 
 /**
  * 全局异常处理
@@ -12,17 +20,53 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @author origami
  * @date 2022/1/17 23:15
  */
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     
+    @ExceptionHandler(Exception.class)
+    public Result<Void> exception(Exception e) {
+        log.debug("未知异常");
+        return Result.failed(e.getMessage());
+    }
+    
+    @ExceptionHandler({ConstraintViolationException.class,
+                       ServletRequestBindingException.class,
+                       BindException.class})
+    public Result<Void> validationException(Exception e) {
+        Result<Void> result = new Result<>();
+        
+        if (e instanceof ConstraintViolationException) {
+            ConstraintViolationException ex =
+                    (ConstraintViolationException) e;
+            
+            return result.setMsg(ex.getConstraintViolations()
+                                   .stream()
+                                   .map(ConstraintViolation::getMessage)
+                                   .collect(Collectors.joining(";")));
+        }
+        
+        if (e instanceof BindException) {
+            BindException ex =
+                    (BindException) e;
+            
+            return result.setMsg(ex.getAllErrors()
+                                   .stream()
+                                   .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                                   .collect(Collectors.joining(";")));
+        }
+    
+        // if (e instanceof ServletRequestBindingException) {
+        //     ServletRequestBindingException ex =
+        //             (ServletRequestBindingException) e;
+        //
+        // }
+    
+        return result;
+    }
+    
     @ExceptionHandler(BaseException.class)
     public Result<Void> baseException(BaseException e) {
-        return new Result<Void>().setCode(e.getCode()).setMsg(e.getMessage());
+        return Result.failed(e.getCode(), e.getMessage());
     }
-    
-    @ExceptionHandler(IllegalParamException.class)
-    public Result<Void> illegalParamException(BaseException e) {
-        return new Result<Void>().setCode(e.getCode()).setMsg(e.getMessage());
-    }
-    
 }
