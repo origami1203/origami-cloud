547160e83c5350cdcd9144558e344d883526d811
origami <mcxyml123@163.com>
2022-02-16 08:11:23 +0800

    更新

--- origami-upm/origami-upm-biz/src/main/java/org/origami/upm/OrigamiUmpApplication.java
+++ origami-upm/origami-upm-biz/src/main/java/org/origami/upm/OrigamiUmpApplication.java
@@ -1,7 +1,9 @@
 package org.origami.upm;
 
+import org.origami.webmvc.EnableWebMvcConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 权限管理biz
@@ -9,7 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author origami
  * @date 2021/12/31 12:03
  */
+@EnableWebMvcConfig
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrigamiUmpApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrigamiUmpApplication.class, args);
