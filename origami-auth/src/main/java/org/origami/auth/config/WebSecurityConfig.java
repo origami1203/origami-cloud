d991b22193e07ce2d0fb3300f82eeb4627ecc72e
苑梦龙 <yuanmenglong@qidu.com>
2022-02-16 18:30:06 +0800

    测试

--- origami-auth/src/main/java/org/origami/auth/config/WebSecurityConfig.java
+++ origami-auth/src/main/java/org/origami/auth/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.origami.auth.service.UserDetailsServiceProvider;
 import org.origami.upm.api.feign.SysUserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -29,95 +30,104 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
  */
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
     @Autowired
     private SysUserFeignClient sysUserFeignClient;
-    
+
     // @Bean
     // @Override
     // public AuthenticationManager authenticationManager() throws Exception {
     //     return super.authenticationManagerBean();
     // }
-    
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-            .antMatchers("/anonymous/**").anonymous()
-            .antMatchers("/swagger-ui/**").anonymous()
-            .antMatchers("/swagger-resources/**").anonymous()
-            .antMatchers("/profile/**").anonymous()
-            .antMatchers("/profile/**").anonymous()
-            .antMatchers("/v3/**").anonymous()
-            .antMatchers("/admin/**").hasRole("ADMIN")
-            .antMatchers("/user/**").hasRole("USER");
+                .antMatchers("login").permitAll()
+                .antMatchers("/oauth/authorize").permitAll()
+                .antMatchers("/exception").permitAll()
+                .antMatchers("/test/**").permitAll()
+                .antMatchers("/open/**").permitAll()
+                .antMatchers("/protect/**").hasRole("USER");
         http.formLogin()
-            .loginProcessingUrl("/login")
-            .successHandler(successHandler())
-            .failureHandler(failureHandler())
-            .permitAll()
-            .and()
-            .logout()
-            .permitAll()
-            .and()
-            .exceptionHandling()
-            .authenticationEntryPoint(authenticationEntryPoint())
-            .accessDeniedHandler(accessDeniedHandler())
-            .and()
-            .authorizeRequests()
-            .anyRequest().authenticated();
+                .loginProcessingUrl("/doLogin")
+                .successHandler(successHandler())
+                .failureHandler(failureHandler())
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint())
+                .accessDeniedHandler(accessDeniedHandler())
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated();
         // http.httpBasic();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
-    
+
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
-           .antMatchers("/**/*.css",
-                        "/**/*.js",
-                        "/swagger-ui/**",
-                        "/swagger-ui.html",
-                        "/swagger-ui/*",
-                        "/swagger-resources/**",
-                        "/v2/api-docs",
-                        "/v3/api-docs",
-                        "/webjars/**");
+                .antMatchers("/**/*.css",
+                             "/**/*.js",
+                             "/swagger-ui/**",
+                             "/swagger-ui.html",
+                             "/swagger-ui/*",
+                             "/swagger-resources/**",
+                             "/v2/api-docs",
+                             "/v3/api-docs",
+                             "/webjars/**");
     }
-    
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+        // auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+        auth.inMemoryAuthentication().withUser("zs").password(passwordEncoder().encode("123456"))
+                .roles("ADMIN")
+                .and()
+                .withUser("ls").password(passwordEncoder().encode("1234"))
+                .roles("USER");
     }
-    
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
     @Bean
     public AuthenticationSuccessHandler successHandler() {
         return new AuthSuccessHandler();
     }
-    
+
     @Bean
     public AuthenticationFailureHandler failureHandler() {
         return new AuthFailureHandler();
     }
-    
+
     @Bean
     public AuthenticationEntryPoint authenticationEntryPoint() {
         return new AuthEntryPoint();
     }
-    
+
     @Bean
     public AccessDeniedHandler accessDeniedHandler() {
         return new AuthAccessDeniedHandler();
     }
-    
+
     @Override
     @Bean
     public UserDetailsService userDetailsService() {
         return new UserDetailsServiceProvider(sysUserFeignClient);
     }
-    
+
 }
