9b7f152cd4050a59cc499502833ce05cdeaa6d3b
origami <mcxyml123@163.com>
2022-02-16 08:09:22 +0800

    更新

--- origami-auth/src/main/java/org/origami/auth/OrigamiAuthApplication.java
+++ origami-auth/src/main/java/org/origami/auth/OrigamiAuthApplication.java
@@ -1,11 +1,18 @@
 package org.origami.auth;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author 28455
  */
+@EnableFeignClients(basePackages = "org.origami")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OrigamiAuthApplication {
     
--- origami-auth/src/main/java/org/origami/auth/config/WebSecurityConfig.java
+++ origami-auth/src/main/java/org/origami/auth/config/WebSecurityConfig.java
@@ -1,34 +1,123 @@
 package org.origami.auth.config;
 
+import org.origami.auth.handler.AuthAccessDeniedHandler;
+import org.origami.auth.handler.AuthEntryPoint;
+import org.origami.auth.handler.AuthFailureHandler;
+import org.origami.auth.handler.AuthSuccessHandler;
+import org.origami.auth.service.UserDetailsServiceProvider;
+import org.origami.upm.api.feign.SysUserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 /**
  * @author origami1203
  * @date 2021-12-23 21:25
  * @description TODO
  */
-@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
+    @Autowired
+    private SysUserFeignClient sysUserFeignClient;
+    
+    // @Bean
+    // @Override
+    // public AuthenticationManager authenticationManager() throws Exception {
+    //     return super.authenticationManagerBean();
+    // }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+            .antMatchers("/anonymous/**").anonymous()
+            .antMatchers("/swagger-ui/**").anonymous()
+            .antMatchers("/swagger-resources/**").anonymous()
+            .antMatchers("/profile/**").anonymous()
+            .antMatchers("/profile/**").anonymous()
+            .antMatchers("/v3/**").anonymous()
+            .antMatchers("/admin/**").hasRole("ADMIN")
+            .antMatchers("/user/**").hasRole("USER");
+        http.formLogin()
+            .loginProcessingUrl("/login")
+            .successHandler(successHandler())
+            .failureHandler(failureHandler())
+            .permitAll()
+            .and()
+            .logout()
+            .permitAll()
+            .and()
+            .exceptionHandling()
+            .authenticationEntryPoint(authenticationEntryPoint())
+            .accessDeniedHandler(accessDeniedHandler())
+            .and()
+            .authorizeRequests()
+            .anyRequest().authenticated();
+        // http.httpBasic();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
-
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+           .antMatchers("/**/*.css",
+                        "/**/*.js",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/swagger-ui/*",
+                        "/swagger-resources/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs",
+                        "/webjars/**");
+    }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+    }
+    
     @Bean
-    PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
+    
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        return new AuthSuccessHandler();
+    }
+    
+    @Bean
+    public AuthenticationFailureHandler failureHandler() {
+        return new AuthFailureHandler();
+    }
+    
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return new AuthEntryPoint();
+    }
+    
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new AuthAccessDeniedHandler();
+    }
+    
+    @Override
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceProvider(sysUserFeignClient);
+    }
+    
 }
