101103d5aea617aeaca3282d0ee65b172ea850d3
origami <mcxyml123@163.com>
2022-02-16 08:08:31 +0800

    新增

---
+++ origami-auth/src/main/java/org/origami/auth/handler/AuthFailureHandler.java
@@ -0,0 +1,35 @@
+package org.origami.auth.handler;
+
+import org.origami.common.core.base.Result;
+import org.origami.common.core.utils.JacksonUtil;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 失败操作
+ *
+ * @author origami
+ * @date 2022/1/21 21:43
+ */
+public class AuthFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException,
+                                                                                  ServletException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        
+        PrintWriter writer = response.getWriter();
+        
+        writer.write(JacksonUtil.toJson(Result.failed("认证失败")));
+        
+    }
+}
