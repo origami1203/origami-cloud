cf429ed7357a4c015a4c1dd736a42c63bdf6a863
苑梦龙 <yuanmenglong@qidu.com>
2022-02-16 18:29:05 +0800

    登录参数

---
+++ origami-auth/src/main/java/org/origami/auth/api/dto/LoginParam.java
@@ -0,0 +1,14 @@
+package org.origami.auth.api.dto;
+
+import lombok.Data;
+
+/**
+ * @author yuanmenglong
+ * @version 1.0.0
+ * @date 2022-02-16 13:40
+ */
+@Data
+public class LoginParam {
+    private String username;
+    private String password;
+}
