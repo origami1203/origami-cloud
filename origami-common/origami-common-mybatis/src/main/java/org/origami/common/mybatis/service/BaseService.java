d6afa1690347863d1f812714019d2068f6da84b8
origami <mcxyml123@163.com>
2023-05-10 20:33:25 +0800

    refactor: 修改方法的接收数据为QueryModel和PageModel

--- origami-common/origami-common-mybatis/src/main/java/org/origami/common/mybatis/service/BaseService.java
+++ origami-common/origami-common-mybatis/src/main/java/org/origami/common/mybatis/service/BaseService.java
@@ -2,18 +2,16 @@ package org.origami.common.mybatis.service;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.PageDTO;
 import com.baomidou.mybatisplus.extension.service.IService;
+import java.util.List;
 import org.origami.common.core.data.page.IPage;
 import org.origami.common.core.data.page.impl.PageImpl;
-import org.origami.common.core.data.query.PageQuery;
-import org.origami.common.core.data.query.Query;
+import org.origami.common.core.data.query.PageModel;
+import org.origami.common.core.data.query.QueryModel;
 import org.origami.common.mybatis.entity.BaseEntity;
 import org.origami.common.mybatis.utils.WrapperUtil;
 
-import java.util.List;
-import org.springframework.cglib.beans.BeanCopier;
-
 /**
- * 自定义基类BaseService
+ * 自定义基类BaseService拓展mp的IService
  *
  * @author origami
  * @date 2022/1/3 17:00
@@ -21,26 +19,26 @@ import org.springframework.cglib.beans.BeanCopier;
 public interface BaseService<T extends BaseEntity> extends IService<T> {
 
     /**
-     * 条件查询
+     * 根据条件查询列表
      *
-     * @param condition 条件
+     * @param queryModel 条件
      * @return {@code List<T>}
      */
-    default List<T> list(Query<T> condition) {
-        return getBaseMapper().selectList(WrapperUtil.getWrapper(condition));
+    default List<T> list(QueryModel<T> queryModel) {
+        return getBaseMapper().selectList(WrapperUtil.getWrapper(queryModel.toQuery()));
     }
 
     /**
      * 条件查询分页
      *
-     * @param condition 条件
+     * @param pageModel 条件
      * @return {@code IPage<T>}
      */
-    default IPage<T> page(PageQuery<T> condition) {
-        PageDTO<T> pageDTO = new PageDTO<>(condition.getPageNum(), condition.getPageSize());
+    default IPage<T> page(PageModel<T> pageModel) {
+        PageDTO<T> pageDTO = new PageDTO<>(pageModel.getPageNum(), pageModel.getPageSize());
         com.baomidou.mybatisplus.core.metadata.IPage<T>
                 page = getBaseMapper().selectPage(pageDTO,
-                WrapperUtil.getWrapper(condition));
+                WrapperUtil.getWrapper(pageModel.toPageQuery()));
 
         return PageImpl.of(Long.valueOf(page.getCurrent()).intValue(),
                 Long.valueOf(page.getSize()).intValue(),
@@ -51,11 +49,11 @@ public interface BaseService<T extends BaseEntity> extends IService<T> {
     /**
      * 条件查询计数
      *
-     * @param condition 条件
+     * @param queryModel 条件
      * @return long
      */
-    default long count(Query<T> condition) {
-        return getBaseMapper().selectCount(WrapperUtil.getWrapper(condition));
+    default long count(QueryModel<T> queryModel) {
+        return getBaseMapper().selectCount(WrapperUtil.getWrapper(queryModel.toQuery()));
     }
 
 }
